93a5e51a69107e29c485e31e1bf70ff43ec3b3d1
EmanuelBerar <emanuel.berar@luminos.software>
2017-08-19 16:59:19 +0300

    initial commit

---
+++ app/services/task/notification_slack.rb
@@ -0,0 +1,10 @@
+class Task::NotificationSlack
+
+  def self.notify_on_slack(task_id, action, task_data_now, task_status)
+    notifier = Slack::Notifier.new 'https://hooks.slack.com/services/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' do
+      defaults channel: '@emanuel.berar',
+                username: "Iliuta Curmezan"
+    end
+      notifier.ping " You have #{action} task with ID: #{task_id}. The description of the task is: '#{task_data_now}' and its status is: #{task_status}"
+  end
+end
